f593d4dd19bd9c9f4dfdd0d78d63b63e9a6c072a
chominju <cmj021002@knu.ac.kr>
2025-04-10 20:27:41 +0900

    style: add font size style

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -14,8 +14,8 @@ const typeStyles = {
 
 const sizeStyles = {
     l: "w-full h-[50px] font-semibold",
-    m: "w-2/3 h-[40px]",
-    s: "w-2/5 h-[40px]",
+    m: "w-2/3 h-[40px] text-sm",
+    s: "w-2/5 h-[40px] text-sm",
 };
 
 export const Button = ({ type = "primary", size = "l", label }: ButtonProps) => {
