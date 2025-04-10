5b0ead455f2dcd8a3264528876a1e0b234229b8f
chominju <cmj021002@knu.ac.kr>
2025-04-10 20:25:52 +0900

    feat: Button 컴포넌트 구현

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+
+interface ButtonProps {
+    type?: "primary" | "secondary" | "login";
+    size?: "l" | "m" | "s";
+    label: string;
+}
+
+const typeStyles = {
+    primary: "bg-point text-white",
+    secondary: "bg-white",
+    login: "bg-yellow",
+};
+
+const sizeStyles = {
+    l: "w-full h-[50px] font-semibold",
+    m: "w-2/3 h-[40px]",
+    s: "w-2/5 h-[40px]",
+};
+
+export const Button = ({ type = "primary", size = "l", label }: ButtonProps) => {
+    const className = clsx("shadow-md rounded-xl", typeStyles[type], sizeStyles[size]);
+
+    return <button className={className}>{label}</button>;
+};
