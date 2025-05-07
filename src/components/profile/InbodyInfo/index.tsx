2cd5f5599b6e3e23635049fce1ed4397948d129c
chominju <cmj021002@knu.ac.kr>
2025-05-07 23:40:56 +0900

    feat: danger type 추가

--- src/components/profile/InbodyInfo/index.tsx
+++ src/components/profile/InbodyInfo/index.tsx
@@ -3,17 +3,20 @@ import { Badge } from "@/components/common/Badge";
 
 interface InbodyInfoProps {
     type: string;
-    label: string;
+    label: "standard" | "danger";
     value: string;
 }
 
-export const InbodyInfo = ({ type, label, value }: InbodyInfoProps) => {
+export const InbodyInfo = ({ type, label = "standard", value }: InbodyInfoProps) => {
     return (
         <div className="flex items-center justify-between p-3 rounded-xl">
             <div className="flex items-center gap-4 ">
                 <img src={point}></img>
                 <span className="text-darkGray">{type}</span>
-                <Badge type="primary" label={label}></Badge>
+                <Badge
+                    type={label === "standard" ? "primary" : "secondary"}
+                    label={label === "standard" ? "표준" : "위험"}
+                ></Badge>
             </div>
             <span className="font-bold">{value}</span>
         </div>
