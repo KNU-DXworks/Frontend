0b4302c2e88b739367c9a7103f949a47c5d03802
chominju <cmj021002@knu.ac.kr>
2025-05-08 20:26:08 +0900

    feat: Post onInterestPostClick, onNotInterestPostClick 추가

--- src/components/common/Post/index.tsx
+++ src/components/common/Post/index.tsx
@@ -10,19 +10,31 @@ interface PostProps {
     label: string;
     text: string;
     postImgUrl: string;
+    onClick?: () => void;
+
+    // onInterestPostClick?: () => void;
+    // onNotInterestPostClick?: () => void;
 }
 
-export const Post = ({ userImgUrl = userIcon, name, time, label, text, postImgUrl }: PostProps) => {
+export const Post = ({ userImgUrl = userIcon, name, time, label, text, postImgUrl, onClick }: PostProps) => {
     const [isExpanded, setIsExpanded] = useState(false);
     const MAX_CHARS = 50;
     const isLong = text.length > MAX_CHARS;
 
+    const onInterestPostClick = () => {
+        console.log("관심 게시물 등록");
+    };
+
+    const onNotInterestPostClick = () => {
+        console.log("관심 없음 게시물 등록");
+    };
+
     return (
         <div className="flex flex-col p-4 rounded-xl shadow-md gap-3">
             {/* 헤더 */}
-            <div className="flex items-center justify-between w-full">
+            <div onClick={onClick} className="flex items-center justify-between w-full">
                 <div className="flex gap-2">
-                    <img src={userImgUrl} className="w-[44px] h-[44px]"></img>
+                    <img src={userImgUrl} className="w-[44px] h-[44px] cursor-pointer"></img>
                     {/* 시간과 사용자  */}
                     <div className="flex flex-col gap-0 cursor-pointer">
                         {/* 사용자이름과 badge */}
@@ -33,7 +45,12 @@ export const Post = ({ userImgUrl = userIcon, name, time, label, text, postImgUr
                         <span className="text-gray text-xs">{time}</span>
                     </div>
                 </div>
-                <SideMenu></SideMenu>
+                <div>
+                    <SideMenu
+                        onInterestPostClick={onInterestPostClick}
+                        onNotInterestPostClick={onNotInterestPostClick}
+                    ></SideMenu>
+                </div>
             </div>
 
             {/* 본문 */}
