16c59cb7d3b9372c4c26b24b06e9eec5c796789e
chominju <cmj021002@knu.ac.kr>
2025-05-08 09:48:45 +0900

    feat: UnregisteredWaller drawer 추가

--- src/components/profile/UnregisteredWallet/index.tsx
+++ src/components/profile/UnregisteredWallet/index.tsx
@@ -1,15 +1,54 @@
 import { Box } from "@/components/common/Box";
 import { Button } from "@/components/common/Button";
+import { Input } from "@/components/common/Input";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
 
-export const UnregisteredWallet = () => {
+interface UnregisteredWalletProps {
+    onClick?: () => void;
+}
+
+export const UnregisteredWallet = ({ onClick }: UnregisteredWalletProps) => {
     return (
-        <Box className="gap-4">
-            <span className="text-darkGray font-bold">나의 지갑</span>
-            <div className="flex flex-col">
-                <span className="text-darkGray text-sm">아직 지갑이 등록되지 않았어요</span>
-                <span className="text-darkGray text-sm">지갑을 등록해보세요</span>
-            </div>
-            <Button type="primary" label="지갑 등록하러 가기"></Button>
-        </Box>
+        <Drawer>
+            <Box className="gap-4 flex-col">
+                <span className="text-darkGray font-bold">나의 지갑</span>
+                <div className="flex flex-col">
+                    <span className="text-darkGray text-sm">아직 지갑이 등록되지 않았어요</span>
+                    <span className="text-darkGray text-sm">지갑을 등록해보세요</span>
+                </div>
+
+                <DrawerTrigger asChild>
+                    <Button type="primary" label="지갑 등록하러 가기" />
+                </DrawerTrigger>
+            </Box>
+
+            <DrawerContent>
+                <DrawerHeader className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-1.5">
+                        <DrawerTitle>지갑 등록</DrawerTitle>
+                        <DrawerDescription className="text-darkGray">등록할 지갑 주소를 입력하세요.</DrawerDescription>
+                    </div>
+
+                    <Input placeholder="자신의 private key를 입력하세요"></Input>
+                </DrawerHeader>
+                <DrawerFooter>
+                    <div className="flex flex-row gap-2">
+                        <Button type="primary" size="m" label="등록 완료" onClick={onClick} />
+                        <DrawerClose asChild>
+                            <Button type="secondary" size="m" label="취소" />
+                        </DrawerClose>
+                    </div>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
     );
 };
