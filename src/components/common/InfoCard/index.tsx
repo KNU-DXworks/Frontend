cbd2c9b2abd45b6ec7f92ebc7c9d6a21319c32db
chominju <cmj021002@knu.ac.kr>
2025-05-08 20:25:35 +0900

    feat: InfoCard onClick props 추가

--- src/components/common/InfoCard/index.tsx
+++ src/components/common/InfoCard/index.tsx
@@ -5,11 +5,15 @@ interface InfoCardProps {
     imgUrl?: string;
     label: string;
     desc: string;
+    onClick?: () => void;
 }
 
-export const InfoCard = ({ type = "primary", imgUrl = userIcon, label, desc }: InfoCardProps) => {
+export const InfoCard = ({ type = "primary", imgUrl = userIcon, label, desc, onClick }: InfoCardProps) => {
     return (
-        <div className="flex flex-col items-center shadow-md p-2 w-[98px] h-[105px] rounded-xl cursor-pointer">
+        <div
+            className="flex flex-col items-center shadow-md p-2 w-[98px] h-[105px] rounded-xl cursor-pointer"
+            onClick={onClick}
+        >
             <img src={imgUrl} className="w-[44px] h-[44px] rounded-full object-contain"></img>
             <span className="text-base">{label}</span>
             {type === "primary" ? (
