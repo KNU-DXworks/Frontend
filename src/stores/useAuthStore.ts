c13a0b11c7d97bb8dc53b05aa007051511728af2
chominju <cmj021002@knu.ac.kr>
2025-05-16 11:19:15 +0900

    feat accessToken, refreshToken store 추가

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,11 +1,12 @@
 import { create } from "zustand";
-
 import { persist } from "zustand/middleware";
 
 interface AuthState {
     isLoggedIn: boolean;
     accessToken: string | null;
     setAccessToken: (token: string) => void;
+    refreshToken: string | null;
+    setRefreshToken: (token: string) => void;
     logout: () => void;
 }
 
@@ -14,16 +15,24 @@ export const useAuthStore = create(
         (set) => ({
             isLoggedIn: false,
             accessToken: null,
+            refreshToken: null,
             setAccessToken: (token: string) => {
                 set({ isLoggedIn: true, accessToken: token });
             },
+            setRefreshToken: (token: string) => {
+                set({ refreshToken: token });
+            },
             logout: () => {
-                set({ isLoggedIn: false, accessToken: null });
+                set({ isLoggedIn: false, accessToken: null, refreshToken: null });
             },
         }),
         {
             name: "auth-storage",
-            partialize: (state: AuthState) => ({ isLoggedIn: state.isLoggedIn, accessToken: state.accessToken }),
+            partialize: (state: AuthState) => ({
+                isLoggedIn: state.isLoggedIn,
+                accessToken: state.accessToken,
+                refreshToken: state.refreshToken,
+            }),
         },
     ),
 );
