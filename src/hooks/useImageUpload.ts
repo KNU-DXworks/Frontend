3680ee2c980eb20c11a8d9572c0617b7617c5429
chominju <cmj021002@knu.ac.kr>
2025-05-17 20:38:32 +0900

    chore: useImageUploader 훅 수정

--- src/hooks/useImageUpload.ts
+++ src/hooks/useImageUpload.ts
@@ -1,11 +1,45 @@
-import { useCallback, useRef, useState } from "react";
+// import { useCallback, useRef, useState } from "react";
+
+// export const useImageUpload = () => {
+//     const [isUploaded, setIsUploaded] = useState<boolean>(false);
+//     const [image, setImage] = useState<File | null>(null);
+//     const [previewURL, setPreviewURL] = useState<string>("");
+//     const [fileType, setFileType] = useState<string>("");
+
+//     const fileInputRef = useRef<HTMLInputElement>(null);
+
+//     const handleImageUpload = useCallback(() => {
+//         fileInputRef.current?.click();
+//     }, []);
+
+//     const handleImageChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+//         const file = e.target.files?.[0] as File;
+//         if (!file) throw new Error("파일이 정상적으로 업로드 되지 않았습니다");
+
+//         setIsUploaded(true);
+//         setImage(file);
+//         setPreviewURL(URL.createObjectURL(file));
+//         setFileType(file.type); // MIME 타입 저장 (예: image/png, application/pdf)
+//     }, []);
+
+//     return {
+//         isUploaded,
+//         image,
+//         previewURL,
+//         fileType,
+//         handleImageUpload,
+//         handleImageChange,
+//         fileInputRef,
+//     };
+// };
+
+// src/entities/images/hooks/useImageUpload.ts
+import { useState, useRef, useCallback } from "react";
 
 export const useImageUpload = () => {
     const [isUploaded, setIsUploaded] = useState<boolean>(false);
     const [image, setImage] = useState<File | null>(null);
     const [previewURL, setPreviewURL] = useState<string>("");
-    const [fileType, setFileType] = useState<string>("");
-
     const fileInputRef = useRef<HTMLInputElement>(null);
 
     const handleImageUpload = useCallback(() => {
@@ -13,22 +47,19 @@ export const useImageUpload = () => {
     }, []);
 
     const handleImageChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0] as File;
-        if (!file) throw new Error("파일이 정상적으로 업로드 되지 않았습니다");
-
+        const file = e.target.files?.[0] ?? null;
+        if (!file) return;
         setIsUploaded(true);
         setImage(file);
         setPreviewURL(URL.createObjectURL(file));
-        setFileType(file.type); // MIME 타입 저장 (예: image/png, application/pdf)
     }, []);
 
     return {
         isUploaded,
         image,
         previewURL,
-        fileType,
+        fileInputRef,
         handleImageUpload,
         handleImageChange,
-        fileInputRef,
     };
 };
