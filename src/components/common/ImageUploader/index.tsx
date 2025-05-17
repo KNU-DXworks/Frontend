c4cd1be02e72067ac34ef6732bfe5427c86c6116
chominju <cmj021002@knu.ac.kr>
2025-05-17 20:37:54 +0900

    chore: ImageUploader 수정

--- src/components/common/ImageUploader/index.tsx
+++ src/components/common/ImageUploader/index.tsx
@@ -1,29 +1,90 @@
+// import { useEffect } from "react";
+
+// import { Plus } from "lucide-react";
+
+// import { useImageUpload } from "@/hooks/useImageUpload";
+
+// import { cn } from "@/lib/utils";
+
+// export interface FileUploadProps {
+//     className?: string;
+//     onChange?: (image: File | null) => void;
+// }
+
+// export const ImageUploader = ({ className, onChange }: FileUploadProps) => {
+//     const { isUploaded, previewURL, image, fileType, fileInputRef, handleImageUpload, handleImageChange } =
+//         useImageUpload();
+
+//     useEffect(() => {
+//         if (image && onChange) {
+//             onChange(image);
+//         }
+//     }, [image, onChange]);
+
+//     return (
+//         <div
+//             className={cn(
+//                 "flex w-full h-[280px] border-2 rounded-xl hover:cursor-pointer justify-center items-center border-darkGray border-dashed",
+//                 className,
+//             )}
+//             onClick={handleImageUpload}
+//         >
+//             {!isUploaded ? (
+//                 <div className="flex flex-col justify-center">
+//                     <Plus className="block mx-auto" size={30} />
+//                     <label>파일 업로드</label>
+//                 </div>
+//             ) : (
+//                 <>
+//                     {fileType.startsWith("image/") && (
+//                         <img
+//                             className="block w-full h-full rounded-[inherit] object-cover"
+//                             src={previewURL}
+//                             alt="uploaded-image"
+//                         />
+//                     )}
+//                     {fileType === "application/pdf" && (
+//                         <embed
+//                             className="block w-full h-full rounded-[inherit] pointer-events-none"
+//                             src={previewURL}
+//                             type="application/pdf"
+//                         />
+//                     )}
+//                 </>
+//             )}
+//             <input
+//                 className="hidden"
+//                 ref={fileInputRef}
+//                 type="file"
+//                 accept="image/*,application/pdf"
+//                 onChange={handleImageChange}
+//             />
+//         </div>
+//     );
+// };
+
+// src/components/common/ImageUploader.tsx
+import { Plus } from "lucide-react";
 import { useEffect } from "react";
 
-import { Plus } from "lucide-react";
-
+import clsx from "clsx";
 import { useImageUpload } from "@/hooks/useImageUpload";
-
-import { cn } from "@/lib/utils";
 
 export interface FileUploadProps {
     className?: string;
-    onChange?: (image: File | null) => void;
+    onChange?: (file: File | null) => void;
 }
 
 export const ImageUploader = ({ className, onChange }: FileUploadProps) => {
-    const { isUploaded, previewURL, image, fileType, fileInputRef, handleImageUpload, handleImageChange } =
-        useImageUpload();
+    const { isUploaded, image, previewURL, fileInputRef, handleImageUpload, handleImageChange } = useImageUpload();
 
     useEffect(() => {
-        if (image && onChange) {
-            onChange(image);
-        }
+        onChange?.(image);
     }, [image, onChange]);
 
     return (
         <div
-            className={cn(
+            className={clsx(
                 "flex w-full h-[280px] border-2 rounded-xl hover:cursor-pointer justify-center items-center border-darkGray border-dashed",
                 className,
             )}
@@ -31,32 +92,20 @@ export const ImageUploader = ({ className, onChange }: FileUploadProps) => {
         >
             {!isUploaded ? (
                 <div className="flex flex-col justify-center">
-                    <Plus className="block mx-auto" size={30} />
+                    <Plus size={30} className="mx-auto" />
                     <label>파일 업로드</label>
                 </div>
+            ) : image?.type.startsWith("image/") ? (
+                <img src={previewURL} alt="uploaded-image" className="w-full h-full object-cover rounded-[inherit]" />
             ) : (
-                <>
-                    {fileType.startsWith("image/") && (
-                        <img
-                            className="block w-full h-full rounded-[inherit] object-cover"
-                            src={previewURL}
-                            alt="uploaded-image"
-                        />
-                    )}
-                    {fileType === "application/pdf" && (
-                        <embed
-                            className="block w-full h-full rounded-[inherit] pointer-events-none"
-                            src={previewURL}
-                            type="application/pdf"
-                        />
-                    )}
-                </>
+                <embed src={previewURL} type="application/pdf" className="w-full h-full rounded-[inherit]" />
             )}
+
             <input
-                className="hidden"
                 ref={fileInputRef}
                 type="file"
                 accept="image/*,application/pdf"
+                className="hidden"
                 onChange={handleImageChange}
             />
         </div>
