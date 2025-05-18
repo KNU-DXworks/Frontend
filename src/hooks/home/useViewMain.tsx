c2f985da98f29af67eda243d472cea713269b277
chominju <cmj021002@knu.ac.kr>
2025-05-18 15:58:13 +0900

    feat: useViewMain 구현

---
+++ src/hooks/home/useViewMain.tsx
@@ -0,0 +1,69 @@
+import { fetchInstance } from "@/app/config/axios";
+import { useQuery } from "@tanstack/react-query";
+
+type BodyType =
+    | "SKINNY"
+    | "SKINNY_MUSCLE"
+    | "STANDARD"
+    | "WEIGHT_LOSS"
+    | "MUSCLE"
+    | "WEIGHT"
+    | "OVERWEIGHT"
+    | "OBESITY"
+    | "MUSCULAR_OBESITY";
+
+interface ViewMainResponseBody {
+    interestUser: [
+        {
+            userId: number;
+            userName: string;
+            profileImg: string;
+            bodyType: BodyType;
+        },
+    ];
+    subscribeUser: [
+        {
+            userId: number;
+            userName: string;
+            profileImg: string;
+            prevType: BodyType;
+            bodyType: BodyType;
+        },
+    ];
+    recommandUser: [
+        {
+            userId: number;
+            userName: string;
+            profileImg: string;
+            prevType: BodyType;
+            bodyType: BodyType;
+        },
+    ];
+    subscribePosts: [
+        {
+            postId: number;
+            userId: number;
+            userName: string;
+            profileImg: string;
+            createdDate: string;
+            bodyType: BodyType;
+            content: string;
+            fileUrl: string;
+            fileType: string;
+        },
+    ];
+}
+
+const viewMain = async () => {
+    const response = await fetchInstance.get<ViewMainResponseBody>("/api/main");
+    return response.data;
+};
+
+export const useViewMain = () => {
+    const query = useQuery({
+        queryKey: ["main"],
+        queryFn: () => viewMain(),
+    });
+
+    return { ...query };
+};
