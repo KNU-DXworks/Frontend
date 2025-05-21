132bce15de51445dbdf3af46a50329b0c3657fdb
chominju <cmj021002@knu.ac.kr>
2025-05-21 19:55:21 +0900

    feat: Input unit 받을 수 있도록

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -5,9 +5,10 @@ interface InputProps {
     placeholder?: string;
     size?: "m" | "s";
     label?: string;
+    unit?: string;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ placeholder, size = "m", label }, ref) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ placeholder, size = "m", label, unit }, ref) => {
     return (
         <div className="flex flex-col gap-3">
             {label && (
@@ -15,15 +16,20 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(({ placeholder, si
                     {label}
                 </label>
             )}
-            <input
-                id="input-field"
-                ref={ref}
-                placeholder={placeholder}
-                className={clsx(
-                    "rounded-xl border border-lightGray h-[55px] p-4 placeholder:text-sm placeholder:text-lightGray outline-none focus:outline-none",
-                    size === "m" ? "w-full" : "w-2/5",
+            <div className="relative w-full">
+                <input
+                    id="input-field"
+                    ref={ref}
+                    placeholder={placeholder}
+                    className={clsx(
+                        "rounded-xl border border-lightGray h-[55px] p-4 pr-12 placeholder:text-sm placeholder:text-lightGray outline-none focus:outline-none",
+                        size === "m" ? "w-full" : "w-2/5",
+                    )}
+                />
+                {unit && (
+                    <span className="absolute right-4 top-1/2 -translate-y-1/2 text-sm text-darkGray">{unit}</span>
                 )}
-            />
+            </div>
         </div>
     );
 });
