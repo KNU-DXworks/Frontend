e5b1952fde2f93b10d2f6243ea8e8ba62cd6baba
chominju <cmj021002@knu.ac.kr>
2025-05-21 19:51:22 +0900

    feat: Box props 상속

--- src/components/common/Box/index.tsx
+++ src/components/common/Box/index.tsx
@@ -4,8 +4,13 @@ import { ReactNode } from "react";
 interface BoxProps {
     className?: string;
     children?: ReactNode;
+    onClick?: () => void;
 }
 
-export const Box = ({ className, children }: BoxProps) => {
-    return <div className={clsx("rounded-xl shadow-md p-4 w-full flex", className)}>{children}</div>;
+export const Box = ({ className, children, onClick }: BoxProps) => {
+    return (
+        <div onClick={onClick} className={clsx("rounded-xl shadow-md p-4 w-full flex", className)}>
+            {children}
+        </div>
+    );
 };
