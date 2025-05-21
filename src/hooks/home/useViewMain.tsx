4dfc872a9cab117063358e3f18b01281a3648e5f
chominju <cmj021002@knu.ac.kr>
2025-05-21 20:06:53 +0900

    feat: useViewMain reponseType 수정

--- src/hooks/home/useViewMain.tsx
+++ src/hooks/home/useViewMain.tsx
@@ -1,6 +1,12 @@
 import { fetchInstance } from "@/app/config/axios";
 import { useQuery } from "@tanstack/react-query";
 
+interface ApiResponse<T> {
+    code: number;
+    message: string;
+    value: T;
+}
+
 type BodyType =
     | "SKINNY"
     | "SKINNY_MUSCLE"
@@ -13,50 +19,43 @@ type BodyType =
     | "MUSCULAR_OBESITY";
 
 interface ViewMainResponseBody {
-    interestUser: [
-        {
-            userId: number;
-            userName: string;
-            profileImg: string;
-            bodyType: BodyType;
-        },
-    ];
-    subscribeUser: [
-        {
-            userId: number;
-            userName: string;
-            profileImg: string;
-            prevType: BodyType;
-            bodyType: BodyType;
-        },
-    ];
-    recommandUser: [
-        {
-            userId: number;
-            userName: string;
-            profileImg: string;
-            prevType: BodyType;
-            bodyType: BodyType;
-        },
-    ];
-    subscribePosts: [
-        {
-            postId: number;
-            userId: number;
-            userName: string;
-            profileImg: string;
-            createdDate: string;
-            bodyType: BodyType;
-            content: string;
-            fileUrl: string;
-            fileType: string;
-        },
-    ];
+    interestUser: {
+        userId: number;
+        userName: string;
+        profileImg: string;
+        bodyType: BodyType;
+    }[];
+    subscribeUser: {
+        userId: number;
+        userName: string;
+        profileImg: string;
+        prevType: BodyType;
+        bodyType: BodyType;
+    }[];
+
+    recommandUser: {
+        userId: number;
+        userName: string;
+        profileImg: string;
+        prevType: BodyType;
+        bodyType: BodyType;
+    }[];
+    subscribePosts: {
+        postId: number;
+        userId: number;
+        userName: string;
+        profileImg: string;
+        createdDate: string;
+        bodyType: BodyType;
+        content: string;
+        fileUrl: string;
+        fileType: string;
+    }[];
 }
 
 const viewMain = async () => {
-    const response = await fetchInstance.get<ViewMainResponseBody>("/api/main");
-    return response.data;
+    const response = await fetchInstance.get<ApiResponse<ViewMainResponseBody>>("/api/main");
+    return response.data.value;
 };
 
 export const useViewMain = () => {
