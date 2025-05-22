331a275ab8b17e95b10a2b1c1eef3ff8c94b8798
chominju <cmj021002@knu.ac.kr>
2025-05-22 16:36:35 +0900

    style: text-leff 적용 및 props ? 추가

--- src/components/profile/UnregisteredWallet/index.tsx
+++ src/components/profile/UnregisteredWallet/index.tsx
@@ -27,7 +27,7 @@ export const UnregisteredWallet = ({ onClick, inputRef }: UnregisteredWalletProp
                     <span className="text-darkGray text-sm">지갑을 등록해보세요</span>
                 </div>
 
-                <DrawerTrigger asChild>
+                <DrawerTrigger className="text-left">
                     <Button type="primary" label="지갑 등록하러 가기" />
                 </DrawerTrigger>
             </Box>
