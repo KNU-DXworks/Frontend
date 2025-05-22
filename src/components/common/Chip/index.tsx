61e2c7d8e565e77294349f25420b7ef57d032a88
chominju <cmj021002@knu.ac.kr>
2025-05-22 10:43:29 +0900

    feat: Chip size option 추가

--- src/components/common/Chip/index.tsx
+++ src/components/common/Chip/index.tsx
@@ -4,14 +4,16 @@ interface ChipProps {
     label: string;
     isSelected?: boolean;
     onClick?: () => void;
+    size?: "s" | "m";
 }
 
-export const Chip = ({ label, isSelected, onClick }: ChipProps) => {
+export const Chip = ({ label, isSelected, onClick, size = "m" }: ChipProps) => {
     return (
         <button
             className={clsx(
-                "flex items-center justify-center border rounded-xl bg-transparent w-fit py-2.5 px-3.5 text-sm",
+                "flex items-center justify-center border rounded-xl bg-transparent w-fit",
                 isSelected ? "border-point text-point" : "border-gray text-gray",
+                size === "m" ? "py-2.5 px-3.5 text-sm" : "py-2 px-2.5 text-xs", // size === "s"
             )}
             onClick={onClick}
         >
