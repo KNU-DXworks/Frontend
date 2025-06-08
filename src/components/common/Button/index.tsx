ba40219f8075005eca8ad2f328248c8e965db349
chominju <cmj021002@knu.ac.kr>
2025-06-08 22:17:38 +0900

    style: telegram option 추가

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,8 +1,8 @@
 import clsx from "clsx";
 
 interface ButtonProps {
-    type?: "primary" | "secondary" | "login" | "gray";
-    size?: "l" | "m" | "s" | "xs";
+    type?: "primary" | "secondary" | "login" | "gray" | "telegram";
+    size?: "l" | "m" | "s" | "xs" | "xxs" | "fit";
     label: string;
     onClick?: () => void;
     children?: React.ReactNode;
@@ -13,6 +13,7 @@ const typeStyles = {
     secondary: "bg-white",
     login: "bg-yellow",
     gray: "bg-chat",
+    telegram: "bg-telegram text-white",
 };
 
 const sizeStyles = {
@@ -20,11 +21,13 @@ const sizeStyles = {
     m: "w-2/3 h-[40px] text-sm",
     s: "w-2/5 h-[40px] text-sm",
     xs: "text-sm w-[100px] p-1.5",
+    xxs: "text-xs w-[100px] p-0.5",
+    fit: "text-sm h-[50px] w-[60px] p-1",
 };
 
 export const Button = ({ type = "primary", size = "l", label, onClick, children }: ButtonProps) => {
     const className = clsx(
-        "shadow-md rounded-xl flex items-center justify-center gap-4",
+        "shadow-md rounded-xl flex items-center justify-center gap-2",
         typeStyles[type],
         sizeStyles[size],
     );
