096312e377a910793b7276e9f21ada3baf9c2ef9
chominju <cmj021002@knu.ac.kr>
2025-06-08 22:28:39 +0900

    feat: privateKey header 추가 및 navigate 추가

--- src/hooks/transaction/useRegisterTransaction.tsx
+++ src/hooks/transaction/useRegisterTransaction.tsx
@@ -3,52 +3,62 @@ import { queryClient } from "@/app/config/query";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { useMutation } from "@tanstack/react-query";
 import { useCallback, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface RegisterTransactionResponseBody {
     privateKey: string;
-    traderId: string;
+    userName: string;
     transactionPeriod: number;
     amount: number;
     info: string;
 }
 
-const registerTransaction = async (data: RegisterTransactionResponseBody) => {
-    const response = await fetchInstance.post("/api/transaction", data);
+const registerTransaction = async (data: RegisterTransactionResponseBody, privateKey: string) => {
+    const response = await fetchInstance.post("/api/transaction", data, {
+        headers: {
+            "X-PRIVATE-KEY": privateKey,
+        },
+    });
     return response.data;
 };
 
 export const useReigsterTransaction = () => {
-    const traderIdRef = useRef<HTMLInputElement | null>(null);
+    const userNameRef = useRef<HTMLInputElement | null>(null);
     const transactionPeriodRef = useRef<HTMLInputElement | null>(null);
     const amountRef = useRef<HTMLInputElement | null>(null);
     const infoRef = useRef<HTMLTextAreaElement | null>(null);
 
     const { privateKey } = useAuthStore();
+    const navigate = useNavigate();
 
     const { mutate } = useMutation({
         mutationFn: () => {
-            if (!privateKey) {
+            if (privateKey === null) {
+                console.log("useRegisterTranscation");
                 throw new Error("private key 정보가 없습니다. 먼저 등록해주세요.");
             }
             const payload = {
                 privateKey,
-                traderId: traderIdRef.current?.value ?? "",
+                userName: userNameRef.current?.value ?? "",
                 transactionPeriod: Number(transactionPeriodRef.current?.value ?? 0),
                 amount: Number(amountRef.current?.value ?? 0),
                 info: infoRef.current?.value ?? "",
             };
 
             console.log(payload);
-            return registerTransaction(payload);
+            return registerTransaction(payload, privateKey);
         },
 
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["transaction"] });
-            alert("성공적으로 저장되었습니다!");
+            queryClient.invalidateQueries({ queryKey: ["main"] });
+            alert("성공적으로 등록되었습니다!");
+            navigate("/");
         },
         onError: (error) => {
             console.error("Error response:", error.message);
-            alert("목표 등록에 실패하였습니다.");
+            alert(error.message);
+            navigate("/profile/my");
         },
     });
 
@@ -57,7 +67,7 @@ export const useReigsterTransaction = () => {
     }, [mutate]);
 
     return {
-        traderIdRef,
+        userNameRef,
         transactionPeriodRef,
         amountRef,
         infoRef,
